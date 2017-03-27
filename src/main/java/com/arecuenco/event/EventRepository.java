d0e90dac85c8f966a6d20866e15c9980c8698692
ARecuenco92 <alvaro.recuenco92@gmail.com>
2017-03-27 22:43:18 +0200

    Added a cache to each repository

--- src/main/java/com/arecuenco/event/EventRepository.java
+++ src/main/java/com/arecuenco/event/EventRepository.java
@@ -1,9 +1,19 @@
 package com.arecuenco.event;
 
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface EventRepository extends CrudRepository<Event, Integer> {
 
+	@Cacheable("eventCache")
+	public Iterable<Event> findAll();
+	
+	@Cacheable("eventCache")
+	public Event findOne(Integer id);
+	
+	@CacheEvict("eventCache")
+	public Event save(Event subscription);
 }
--- src/main/java/com/arecuenco/subscription/SubscriptionRepository.java
+++ src/main/java/com/arecuenco/subscription/SubscriptionRepository.java
@@ -2,11 +2,17 @@ package com.arecuenco.subscription;
 
 import java.util.List;
 
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface SubscriptionRepository extends CrudRepository<Subscription, Integer> {
 
+	@Cacheable("subscriptionCache")
 	public List<Subscription> findByNewsletterId(Integer newsletterId);
+
+	@CacheEvict("subscriptionCache")
+	public Subscription save(Subscription subscription);
 }
